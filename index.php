226bc930af71f87abeb34c6bf641d5b66f72d641
faitholysis <faith.adegoke1@gmail.com>
2022-09-04 23:30:30 +0100

    test_version1.0

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<title>NERIT ...the all round solution network...</title>
+</head>
+<body>
+<div class="flag">
+</div>
+
+</body>
+</html>
